c6f1e0ce78acd4f9daf10feda82a043c0700daed
Cesar <ccordero.007@gmail.com>
2025-07-13 10:30:51 -0600

    Adding a custom Logger

--- main.go
+++ main.go
@@ -1,17 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+
+	"cesarcordero.com/go/movieapp/logger"
 )
+
+func initializeLogger() *logger.Logger {
+	logInstance, err := logger.NewLogger("movie.log")
+	if err != nil {
+		log.Fatalf("Failed to initialice logger: %v", err)
+	}
+	defer logInstance.Close()
+	return logInstance
+}
 
 func main() {
 
+	logInstance := initializeLogger()
+
 	http.Handle("/", http.FileServer(http.Dir("public")))
+	fmt.Println("Serving the files")
 
 	const addr = ":8080"
 	err := http.ListenAndServe(addr, nil)
 	if err != nil {
 		log.Fatalf("Server failed: %v", err)
+		logInstance.Error("Server failed", err)
 	}
 }
