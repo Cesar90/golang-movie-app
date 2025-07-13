b646a8638f24fec4b6b5af94f7c336d793f7dd66
Cesar <ccordero.007@gmail.com>
2025-07-13 13:51:43 -0600

    Interface to handle different connections maybe postgres, mysql or wharever

---
+++ data/interfaces.go
@@ -0,0 +1,11 @@
+package data
+
+import "cesarcordero.com/go/movieapp/models"
+
+type MovieStorage interface {
+	GetToMovies() ([]models.Movie, error)
+	GetRandomMovies() ([]models.Movie, error)
+	GetMoviesById(id int) (models.Movie, error)
+	SearchMoviesByName(name string) ([]models.Movie, error)
+	GetAllGenres() ([]models.Genre, error)
+}
